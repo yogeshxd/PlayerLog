a4af95b339d04a17dd74ea5f3b4846efba278274
DuB <45172882+DuB-DeVS@users.noreply.github.com>
2020-12-19 18:13:36 +0530

    Update status.py

--- cogs/status.py
+++ cogs/status.py
@@ -10,7 +10,7 @@ class Status(commands.Cog, name='Status'):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(brief='xirp status', description='Give the number of connected players.')
+    @commands.command(brief='-status', description='Give the number of connected players.')
     async def status(self, ctx):
         await ctx.channel.purge(limit=1)
         stat = True
@@ -31,25 +31,25 @@ class Status(commands.Cog, name='Status'):
             maxcl = info["sv_maxClients"]
             connected = info["sv_queueConnectedCount"]
             temp=discord.Embed(title="Server Status", description=f"Server is up having {connected}/{maxcl} players and {queue} players are still in queue.", color=random.randint(0, 0xffffff))
-            temp.set_footer(text="Xtreme India", icon_url="https://cdn.discordapp.com/attachments/710804221410410616/749240202962141244/xirp-new.png")
             msg = await ctx.channel.send(embed=temp, delete_after=10)
         else:
             temp=discord.Embed(title="Server Status", description="**Seems like server is offline...**", color=random.randint(0, 0xffffff))
-            temp.set_footer(text="Xtreme India", icon_url="https://cdn.discordapp.com/attachments/710804221410410616/749240202962141244/xirp-new.png")
             msg = await ctx.channel.send(embed=temp, delete_after=10)
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
     async def players(self, ctx):
-        name = ['DuB', 'Girish', 'Carter', 'Shetty', 'Krishna']
-        admin = [626811868249325578, 385733388872712192, 400241486886600714, 383977117479600131, 549679174890029058]
+        #Next two lines are to filter particular admins...
+        #name = ['DuB']
+        #admin = [626811868249325578]
         author = ctx.message.author
         cit = ''
         with open('ip.txt', 'rb') as ip:
             url = ip.readlines()
             nurl = str(url[0])[2:len(url[0])+2]
             ip.close
-        if author.id in admin:
+        #if author.id in admin:
+        if True:
             try:
                 i = requests.get(f"{nurl}info.json")
                 inf = i.json()
